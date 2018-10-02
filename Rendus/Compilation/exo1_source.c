47d56ede374274a972b1f3d4be95ee9ce3a85fbc
Enrique Gomez <enrique.gomez@telecom-paristech.fr>
2018-10-02 03:41:58 +0200

    Edition of exo1 and exo2 files

--- Rendus/Compilation/exo1_source.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-char *p = "Jello World!\n";
-int main()
-{
-    p[0] = 'H';
-    printf("%s", p);
-    return 0;
-}
---
+++ Rendus/executables/exo1/file3.c
@@ -0,0 +1,16 @@
+#include <stdint.h>
+#include <stdio.h>
+int32_t x = 34;
+int64_t y;
+const char mesg[] = "Hello World!";
+int main()
+{
+    static uint8_t z;
+    uint16_t t;
+    y = 12;
+    z = z + 1;
+    t = y + z;
+    printf(mesg);
+    printf("x = %d, y = %d, z = %d, t = %d\n", x, y, z, t);
+    return 0;
+}
