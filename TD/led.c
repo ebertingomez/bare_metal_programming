983c08160b41b1156fb143d400d7c94106aa32db
Enrique Gomez <enrique.gomez@telecom-paristech.fr>
2018-10-08 20:04:32 +0200

    First Edition of the function which controls the yellow and blue LED

--- TD/led.c
+++ TD/led.c
@@ -5,6 +5,9 @@
 #define GPIOB5_MODE_REG (*(volatile uint32_t *)0x48000400)
 #define GPIOB5_VALUE_REG (*(volatile uint32_t *)0x48000418)
 
+#define PC9_MODE_REG (*(volatile uint32_t *)0x48000800)
+#define PC9_VALUE_REG (*(volatile uint32_t *)0x48000818)
+
 enum state
 {
     LED_OFF = 0,
@@ -16,14 +19,35 @@ void led_init()
 {
     GPIO_CLOCK_REG |= (1 << 1);
     GPIOB5_MODE_REG = (GPIOB5_MODE_REG | (1 << 28)) & ~(1 << 29);
+
+    PC9_MODE_REG = (PC9_MODE_REG | (1 << 18)) & ~(1 << 19);
 }
 
 void led_g_on()
 {
-    GPIOB5_VALUE_REG = (GPIOB5_VALUE_REG & ~(1<<30)) | (1<<14);
+    GPIOB5_VALUE_REG |= (1 << 14);
 }
 void led_g_off()
 {
-    GPIOB5_VALUE_REG = (GPIOB5_VALUE_REG & ~(1<<14)) | (1<<30);
+    GPIOB5_VALUE_REG |= (1 << 30);
 }
-//TODO: void led(int a){}
+void led(int s)
+{
+
+    switch (s)
+    {
+    case LED_OFF:
+        PC9_VALUE_REG = PC9_VALUE_REG;
+        break;
+    case LED_YELLOW:
+        PC9_VALUE_REG |= (1 << 9);
+        break;
+
+    case LED_BLUE:
+        PC9_VALUE_REG |= (1 << 25);
+        break;
+
+    default:
+        break;
+    }
+}
