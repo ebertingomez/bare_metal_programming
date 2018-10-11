3ab6d884de7d9e7ebfb7438c99cae13b16f559b2
Enrique Gomez <enrique.gomez@telecom-paristech.fr>
2018-10-11 17:55:02 +0200

    Completion of led.c with the right macros

--- TD/led.c
+++ TD/led.c
@@ -1,28 +1,22 @@
 #include "led.h"
 #include <stdint.h>
-
-#define GPIO_CLOCK_REG (*(volatile uint32_t *)0x4002104c)
-#define GPIOB5_MODE_REG (*(volatile uint32_t *)0x48000400)
-#define GPIOB5_VALUE_REG (*(volatile uint32_t *)0x48000418)
-
-#define PC9_MODE_REG (*(volatile uint32_t *)0x48000800)
-#define PC9_VALUE_REG (*(volatile uint32_t *)0x48000818)
+#include "stm32l475xx.h"
 
 void led_init()
 {
-    GPIO_CLOCK_REG |= (3 << 1);
-    GPIOB5_MODE_REG = (GPIOB5_MODE_REG | (1 << 28)) & ~(1 << 29);
+    RCC->AHB2ENR |= (RCC_AHB2ENR_GPIOBEN | RCC_AHB2ENR_GPIOCEN);
+    GPIOB->MODER = (GPIOB->MODER & ~GPIO_MODER_MODE14_Msk) | GPIO_MODER_MODE14_0 ;
 
-    PC9_MODE_REG = (PC9_MODE_REG | (1 << 18)) & ~(1 << 19);
+    GPIOC->MODER = (GPIOC->MODER & ~GPIO_MODER_MODE9_Msk) | GPIO_MODER_MODE9_0;
 }
 
 void led_g_on()
 {
-    GPIOB5_VALUE_REG |= (1 << 14);
+    GPIOB->BSRR |= GPIO_BSRR_BS_14;
 }
 void led_g_off()
 {
-    GPIOB5_VALUE_REG |= (1 << 30);
+    GPIOB->BSRR |= GPIO_BSRR_BR_14;
 }
 void led(state s)
 {
@@ -30,13 +24,13 @@ void led(state s)
     switch (s)
     {
     case LED_OFF:
-        PC9_MODE_REG &= ~(3 << 18);
+        GPIOC->MODER &= ~GPIO_MODER_MODE9_Msk;
         break;
     case LED_YELLOW:
-        PC9_VALUE_REG |= (1 << 9);
+        GPIOC->BSRR |= GPIO_BSRR_BS_9;
         break;
     case LED_BLUE:
-        PC9_VALUE_REG |= (1 << 25);
+        GPIOC->BSRR |= GPIO_BSRR_BR_9;
         break;
     default:
         break;
