80d3729826bd2db97c1b38c7f28ea24325033dd0
Enrique Gomez <enrique.gomez@telecom-paristech.fr>
2018-10-12 11:18:46 +0200

    Completion of uart_init()

--- TD/led.c
+++ TD/led.c
@@ -1,21 +1,23 @@
 #include "led.h"
 #include "stm32l475xx.h"
+#include "stm32l4xx.h"
 
 void led_init()
 {
-    RCC->AHB2ENR |= (RCC_AHB2ENR_GPIOBEN | RCC_AHB2ENR_GPIOCEN);
+    SET_BIT(RCC->AHB2ENR, RCC_AHB2ENR_GPIOBEN);
+    SET_BIT(RCC->AHB2ENR, RCC_AHB2ENR_GPIOCEN);
+    
     GPIOB->MODER = (GPIOB->MODER & ~GPIO_MODER_MODE14_Msk) | GPIO_MODER_MODE14_0;
-
     GPIOC->MODER = (GPIOC->MODER & ~GPIO_MODER_MODE9_Msk) | GPIO_MODER_MODE9_0;
 }
 
 void led_g_on()
 {
-    GPIOB->BSRR |= GPIO_BSRR_BS_14;
+    SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS_14);
 }
 void led_g_off()
 {
-    GPIOB->BSRR |= GPIO_BSRR_BR_14;
+    SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR_14);
 }
 void led(state s)
 {
@@ -23,13 +25,13 @@ void led(state s)
     switch (s)
     {
     case LED_OFF:
-        GPIOC->MODER &= ~GPIO_MODER_MODE9_Msk;
+        CLEAR_BIT(GPIOC->MODER, GPIO_MODER_MODE9_Msk);
         break;
     case LED_YELLOW:
-        GPIOC->BSRR |= GPIO_BSRR_BS_9;
+        SET_BIT(GPIOC->BSRR, GPIO_BSRR_BS_9);
         break;
     case LED_BLUE:
-        GPIOC->BSRR |= GPIO_BSRR_BR_9;
+        SET_BIT(GPIOC->BSRR, GPIO_BSRR_BR_9);
         break;
     default:
         break;
--- TD/uart.h
+++ TD/uart.h
@@ -3,9 +3,9 @@
 #include <stdint.h>
 #include <stddef.h>
 
-void uart_init();
+void uart_init(void);
 void uart_putchar(uint8_t c);
-uint8_t uart_getchar();
+uint8_t uart_getchar(void);
 void uart_puts(const uint8_t *s);
 void uart_gets(uint8_t *s, size_t size);
 
