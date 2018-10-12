2806b51854e1d3d0315e1df190cdc659aa67cae6
Enrique Gomez <enrique.gomez@telecom-paristech.fr>
2018-10-12 11:53:25 +0200

    WIP

--- TD/uart.c
+++ TD/uart.c
@@ -11,21 +11,34 @@ void uart_init()
     GPIOB->AFR[0] = (GPIOB->AFR[0] & GPIO_AFRL_AFRL7) & ~GPIO_AFRL_AFSEL7_3;
 
     /* Clocks initialization ; PCLK=80MHz*/
-    SET_BIT(RCC->APB2ENR,RCC_APB2ENR_USART1EN);
-    CLEAR_BIT(RCC->CCIPR,RCC_CCIPR_USART1SEL);
+    SET_BIT(RCC->APB2ENR, RCC_APB2ENR_USART1EN);
+    CLEAR_BIT(RCC->CCIPR, RCC_CCIPR_USART1SEL);
 
     /* Serial port initialization */
-    SET_BIT(RCC->APB2RSTR,RCC_APB2RSTR_USART1RST);
+    SET_BIT(RCC->APB2RSTR, RCC_APB2RSTR_USART1RST);
     WRITE_REG(USART1->BRR, 694);
     WRITE_REG(USART1->CR1, 0);
     WRITE_REG(USART1->CR2, 0);
     USART1->CR1 |= USART_CR1_TE | USART_CR1_RE;
 }
+
 void uart_putchar(uint8_t c)
-{ //TODO
+{
+
+    while (READ_BIT(USART1->ISR, USART_ISR_TXE) != 0)
+    {
+    }
+            WRITE_REG(USART1->TDR, c);
+
 }
 uint8_t uart_getchar()
-{ //TODO
+{ 
+    while (READ_BIT(USART1->ISR, USART_ISR_RXNE) != 0)
+    {
+        
+    }
+    return READ_REG(USART1->RDR);
+
 }
 void uart_puts(const uint8_t *s)
 { //TODO
