644886f0c076248c5ed03aa2db5e6634d5466797
Enrique <enrique.gomez@telecom-paristech.fr>
2018-10-25 23:00:12 +0200

    Beginning Flash. Setting up of clocks.c, crt0.s, Makefile and .gdbinit

--- TD/crt0.s
+++ TD/crt0.s
@@ -1,5 +1,6 @@
 .global _start
 .syntax unified
+.thumb_func
 
 /*  This file is used to make the link edition of object files in order to create
     a executable. It set the stack pointer and calls a function to empty the bss. */
